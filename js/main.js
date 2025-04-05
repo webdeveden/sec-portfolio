1feb09527c416cab3af00aa13d67c1ba6ee521dc
Eden Abwe <edenjardin089@gmail.com>
2025-04-05 16:21:10 -0400

    applying some javascript on the hero__box class and change hero__experience to hero__class

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 const navToggler = document.querySelector(".nav__toggler");
 const collapsible = document.querySelector(".collapsible");
 
+const hero = document.querySelectorAll(".hero__box");
+// const heroExperience = document.querySelectorAll(".hero__experience")
+hero.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    this.classList.add("outsider"); // Add class on hover
+  });
+
+  item.addEventListener("mouseleave", function () {
+    this.classList.remove("outsider"); // Remove class when mouse leaves
+  });
+});
+
 navToggler.addEventListener("click", function () {
   collapsible.classList.toggle("collapsible--expanded");
 });
